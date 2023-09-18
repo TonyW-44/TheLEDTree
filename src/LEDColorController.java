b5b88ca329b52ef5f3905b8a05d29053142ccfd1
LiviMcK <137836813+LiviMcK@users.noreply.github.com>
2023-09-18 14:31:45 -0700

    Potential color change code using raspberry pi

--- src/LEDColorController.java
+++ src/LEDColorController.java
@@ -4,18 +4,18 @@ import java.util.concurrent.TimeUnit; // this is a time related operation for sl
 public class LEDColorController { // Public or private?
     public static void main(String[] args) throws InterruptedException { // throws InterruptedException for color change
         GpioController gpio = GpioFactory.getInstance();
-        GpioPinDigitalOutput colorPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Color", PinState.LOW);
-        GpioPinDigitalOutput colorPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "Color", PinState.LOW);
-        GpioPinDigitalOutput colorPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "Color", PinState.LOW);
+        GpioPinDigitalOutput redPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Red", PinState.LOW);
+        GpioPinDigitalOutput bluePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "Blue", PinState.LOW);
+        GpioPinDigitalOutput greenPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "Green", PinState.LOW);
 
         try {
             // Change LED colors in a loop, we can change this to fit.
             while (true) {
-                colorPin.toggle();
+                redPin.toggle();
                 TimeUnit.SECONDS.sleep(1); // all three of these are pausing for 1 second before changing
-                colorPin.toggle(); // Have to choose colors or repeat this for more color options
+                bluePin.toggle(); // Have to choose colors or repeat this for more color options
                 TimeUnit.SECONDS.sleep(1); // change second to fit whatever we want
-                colorPin.toggle();
+                greenPin.toggle();
                 TimeUnit.SECONDS.sleep(1);
             }
         } finally {
